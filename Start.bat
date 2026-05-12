200bce5845263c88e7d2957b1a80bd610be819c5
DenisPoselyanov <ternopil62000@gmail.com>
2026-05-12 14:43:53 +0300

    Змінив токен

--- Start.bat
+++ Start.bat
@@ -1,4 +1,4 @@
 @echo off
-set BOT_TOKEN=8226488857:AAHKC5W7HVUsFo0mgaFO44NAQ8TYMrZHOCE
+set BOT_TOKEN=8226488857:AAGU-7Qc-hqvxSe03rTRnacH3znSf5kmMQY
 C:\Users\PoselyanovLaptop\AppData\Local\Programs\Python\Python311\python.exe bot.py
 pause
